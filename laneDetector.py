5e51e6014c7082a6e7b8c52e069cea4972b544b4
Unknown <siamak.esmi@gmail.com>
2017-06-08 17:49:08 +0200

    adaptive_HScan verified/added

--- laneDetector.py
+++ laneDetector.py
@@ -33,42 +33,106 @@ startTime=time.time()
 processed_img = get_image()
 
 # devide the frame into 4 separate sub-frame 	#(260, 640)
-bottom_right_part = processed_img[processed_img.shape[0]//2 : processed_img.shape[0], processed_img.shape[1]//2 : processed_img.shape[1]]
-bottom_left_part = processed_img[processed_img.shape[0]//2 : processed_img.shape[0], 0 : processed_img.shape[1]//2 -1]
 top_right_part = processed_img[0 : processed_img.shape[0]//2-1, processed_img.shape[1]//2 : processed_img.shape[1]]
 top_left_part = processed_img[0 : processed_img.shape[0]//2-1, 0 : processed_img.shape[1]//2 -1]
+bottom_left_part = processed_img[processed_img.shape[0]//2 : processed_img.shape[0], 0 : processed_img.shape[1]//2 -1]
+bottom_right_part = processed_img[processed_img.shape[0]//2 : processed_img.shape[0], processed_img.shape[1]//2 : processed_img.shape[1]]
 
-print('a row of a binarized frame: %s'% bottom_right_part[0])
+#print('a row of a binarized frame: %s'% bottom_right_part[0])
 # Find the righ line candidate
 
-def HScan(frame): # , first_white_pixel_x , offset):
+
+def adaptive_HScan(frame, offset): # , first_white_pixel_x , offset):
 	'''
-	Scans a row of a binary image and returns the coordinations of wihte pixels
-	Input: a binary list 
+	Scans a binary frame horizontally and returns the coordinations of wihte pixels
+	Input: numpy.ndarray
 	Output: a list of white pixels
-			nonZeroCoordinates: column number of transition from black to white and vice versa, i.e. edges!
-			binarizedRow: almost tha same as row but begining and end of the white pixels
-			are turned to 1
+
 	'''
-	row_derivative = np.diff(frame[0])
-	print row_derivative
-	ind_row_derivative = np.nonzero(row_derivative)
+	print 'image shape: %s' %(frame.shape,)
+	frame
+	frame_white_pixels = []
+	white_pixels = []
+	assigned_FWP = False
+	# find derivative of entire frame
+	frame_derivative = np.diff(frame)
+	nRows = frame_derivative.shape[0]-1 # in order to start from bottom..
 	
-	return ind_row_derivative
+	for i in xrange(nRows):
+		row = nRows-i
+		if not assigned_FWP:
+			# find the position of the edges
+			ind_row_derivative = np.nonzero(frame_derivative[row]) # returns a tuple
+			for p in xrange(2): #len(ind_row_derivative[0]  # two first lines
+				white_pixels.append((ind_row_derivative[0][p], row))
+
+			first_white_pixel_x = white_pixels[0][0]  # neglrct the +1 position for now..
+			assigned_FWP = True
+		else:
+			start_pixel = first_white_pixel_x + offset
+			ind_row_derivative = np.nonzero(frame_derivative[row][start_pixel:])
+			for p in xrange(2): #len(ind_row_derivative[0])
+				white_pixels.append((ind_row_derivative[0][p]+start_pixel, row))
+			first_white_pixel_x = white_pixels[0][0]  # neglrct the +1 position for now..
+			assigned_FWP = True
+		frame_white_pixels.append(white_pixels)
+		white_pixels = []
+	return frame_white_pixels
+
+def adaptive_VScan(frame, offset): # , first_white_pixel_x , offset):
+	'''
+	Scans a binary frame vertically and returns the coordinations of wihte pixels
+	Input: numpy.ndarray
+	Output: a list of white pixels
+
+	'''
+	
+	frame_white_pixels = []
+	white_pixels = []
+	assigned_FWP = False
+	# find derivative of entire frame
+	frame_derivative = np.diff(frame)
+
+	nCols = frame_derivative.shape[1]
+	for i in xrange(0,nCols):
+		
+		if not assigned_FWP:
+			# find the position of the edges
+			column = frame_derivative[:,i]
+			reversed_column = column[::-1]
+			
+			ind_col_derivative = np.nonzero(reversed_column) # returns a tuple
+			if (len(ind_col_derivative)!=0):
+				for p in xrange(2): #len(ind_row_derivative[0]  # two first lines
+					white_pixels.append((ind_col_derivative[0][p]),i)
+			else:
+				continue		
+			first_white_pixel_y = white_pixels[0][0]  # neglrct the +1 position for now..
+			assigned_FWP = True
+		else:
+			start_pixel = first_white_pixel_y + offset  #negative offset
+			reversed_column = frame_derivative[start_pixel:,i] # ready for optimiziation..
+			ind_col_derivative = np.nonzero(reversed_column)
+			if (len(ind_col_derivative)!=0):
+				for p in xrange(2): #len(ind_row_derivative[0])
+					white_pixels.append((ind_col_derivative[0][p]+start_pixel, i))
+			else:
+				continue
+			first_white_pixel_y = white_pixels[0][0]  # neglrct the +1 position for now..
+			assigned_FWP = True
+		frame_white_pixels.append(white_pixels)
+		white_pixels = []
+	return frame_white_pixels
 
 
-
-
-
-
-
-
-
-
-
-
-d = HScan(bottom_right_part)
-print d[0]
+wpixel = adaptive_VScan(top_left_part, -3)
+for rows in wpixel:
+	for points in rows:
+		cv2.circle(bottom_right_part, points,2,(155,155,155))
 print('executionTime: %s' %(time.time() - startTime))
-cv2.imshow('image', top_right_part)
+cv2.imshow('image',top_left_part)
 cv2.waitKey(0)
+
+"""
+
+"""
