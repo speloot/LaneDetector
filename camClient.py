22ad33b98628f2a4a9efbc4b2d6b82a34c4b3740
Unknown <siamak.esmi@gmail.com>
2017-06-20 13:51:14 +0200

    functioning server/client

--- camClient.py
+++ camClient.py
@@ -5,28 +5,28 @@ import socket
 import time
 import io
 import struct
-from PIL import Image, ImageTk
+import cStringIO
 import Tkinter as tk
+from PIL import Image, ImageTk
 '''
 camClient receives images from camera server
 '''
+def button_click_exit_mainloop (event):
+    event.widget.quit() # this will cause mainloop to unblock.
 
-picSize = (640,480)
 root = tk.Tk()
-root.geometry('%dx%d+%d+%d' %(640,480,100,100))
-
-
-tkpi = ImageTk.PhotoImage(Image.new('RGB', picSize))
-
-label_image = tk.Label(root, image=tkpi)
-label_image.pack()
+root.geometry("640x480")
+root.wm_title("pi camera")
+root.bind("<Button>", button_click_exit_mainloop)
 root.mainloop()
 
-client_socket = socket.socket()
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+tk_img = ImageTk.PhotoImage(Image.new('RGB', (640, 480)))
+panel = tk.Label(root, image=tk_img)
+panel.place(x=0,y=0,width=640,height=480)
 
 # connect to the server
+client_socket = socket.socket()
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 client_socket.connect(('192.168.75.13', 7798))
 data_file = client_socket.makefile()
 frame_stream = io.BytesIO()
@@ -43,14 +43,11 @@ while True:
 	frame_stream.seek(0)
 	frame_stream.write(data_file.read(frame_len))
 	frame_stream.seek(0)
+	print 'Image is received!'
 	img = Image.open(frame_stream)
-	
-	
 	img.load()
 	img.verify()
-
-	img_tk = ImageTk.PhotoImage(image=img)
-	label_image.img_tk = img_tk
-	label_image.configure(image=img_tk)
-	
-	
+	tk_img = ImageTk.PhotoImage(img.resize((640, 480),Image.ANTIALIAS))
+	panel.configure(image=tk_img)
+	root.update()
+	print('\nElapsed Time: \n%s\n' %(time.time()-start_time))
--- camServer.py
+++ camServer.py
@@ -12,7 +12,7 @@ cam server sends images fron picamera to a client
 # create a socket object
 server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('', port))
+server_socket.bind(('', 7798))
 server_socket.listen(0)
 
 client_socket, addr = server_socket.accept()
@@ -28,19 +28,18 @@ with picamera.PiCamera() as camera:
 	cam_stream = io.BytesIO()
 
 	for foo in camera.capture_continuous(cam_stream, 'jpeg', True):
+		startTime = time.time()
 		cam_stream.seek(0)
-        frame = cam_stream.read()
-        cam_stream.seek(0)
+		frame = cam_stream.read()
+		cam_stream.seek(0)
 		cam_stream.truncate()
-		
 		dataFile.write(struct.pack('<L', len(frame)))
 		dataFile.flush()
 
 		dataFile.write(frame)
 		dataFile.flush()
 		print'Image Is Sent!'
-	#print('\nelapsed Time: \n%s\n' %(time.time()-startTime))
-
+		print('\nElapsed Time: \n%s\n' %(time.time()-startTime))
 
 
 
